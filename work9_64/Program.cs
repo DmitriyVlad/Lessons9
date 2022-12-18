8d2a507655b7f2f0ba7e45821bbbdc3daac8970d
Dmitriy <timon@yahoo.com>
2022-12-18 11:53:33 +0300

    Задание 64

---
+++ work9_64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64. Вывести натуральные числа от N до 1 с помощью рекурсии
+
+Console.Write("Введите натуральное число больше 1: ");
+int number = int.Parse(Console.ReadLine());
+
+void NumberCounter (int number)
+{
+    if (number == 0) return;
+    Console.Write($"{number} ");
+    NumberCounter (number - 1);
+}
+
+NumberCounter(number);
+
+Console.WriteLine();
